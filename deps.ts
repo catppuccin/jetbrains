f35fa51abd01639f654fad0ff96b683d348f585d
winston <hey@winston.sh>
2022-09-10 09:28:34 +0200

    feat: 0.2.0 XMLs

--- deps.ts
+++ deps.ts
@@ -1,2 +1,4 @@
 export type { JetBrains } from "./types.ts";
-export { variants } from "https://esm.sh/@catppuccin/palette@0.1.5";
+export { labels, variants } from "https://esm.sh/@catppuccin/palette@0.1.5";
+import Handlebars from "https://esm.sh/handlebars@4.7.7";
+export { Handlebars };
