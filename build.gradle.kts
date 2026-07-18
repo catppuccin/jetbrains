71b78a50557d87af64ec75bb1b1bdfa17f416ed8
Hammy <58985301+sgoudham@users.noreply.github.com>
2026-07-18 21:50:58 +0100

    chore: bump deps & support 2026.2 (#273)

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,9 @@ plugins {
     // Java support
     id("java")
     // Kotlin support
-    id("org.jetbrains.kotlin.jvm") version "2.3.20"
+    id("org.jetbrains.kotlin.jvm") version "2.4.10"
     // Gradle IntelliJ Platform Plugin
-    id("org.jetbrains.intellij.platform") version "2.14.0"
+    id("org.jetbrains.intellij.platform") version "2.18.1"
     // Gradle Changelog Plugin
     id("org.jetbrains.changelog") version "2.5.0"
 }
